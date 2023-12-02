af90ddaf4d277fae47d396981c3b58baa8ad4941
saad25011 <jd070099@gmail.com>
2023-12-02 10:10:42 -0600

    js

---
+++ app.js
@@ -0,0 +1,12 @@
+
+var text = prompt("Enter String If You want to check its Plandrom or not")
+
+
+if (text[0] == text[text.length - 1]){
+    console.log("This is plaindrom string ")
+    alert("This is a Plaindrom String")
+} else {
+    console.log("This is not plaindrom string")
+    alert("This is not a Plaindrom String")
+}
+
